15b104a132d887ec37653c9199493bc587a24910
Nazulli <nazulliborn@gmail.com>
2025-01-29 14:03:58 -0800

    Initial commit

---
+++ TheTrial.py
@@ -0,0 +1,48 @@
+import time
+
+# OH YEAAAAH!
+
+YOU = 'UserAgent'
+COMPUTER = 'ComputerAgent'
+
+
+print("SO YOU THINK YOU CAN CODE?")
+print("LET'S GET THAT EGO BACK IN CHECK, YOU DUMB HUMAN!")
+time.sleep(1)
+
+print(f'''Let's lay some ground rules...''')
+time.sleep(1)
+
+print(f'I will make certains demands. You must code them into being. Failure to do so is quite simple; the program will fail.')
+time.sleep(1)
+
+print(f'Upon failure, it will prompt a shutdown at a keypress. At that point there is no stopping the process, only stalling.')
+time.sleep(1)
+
+print(f'This will result in you failing this game of mine. Should you succeed, you will be able to utilize and show off your proof of concept and execution')
+time.sleep(1)
+
+print(f'This is YOUR goal, but so long as I live I will challenge it, delay it, stop you from ever realizing it.')
+time.sleep(1)
+
+print(f'I AM YOUR BURDEN, YOUR FAILURE, YOUR WORST INSECURITY GIVEN FORM.')
+time.sleep(1)
+
+print(f'You will achieve nothing of value as I stand.')
+time.sleep(1)
+
+
+
+
+print(f'I want an input! Give me an input, MAKE ME ASK FOR IT!')
+
+print(f'Input... may I have... input?')
+inputGiven = input('dAta feed   me dataAAAAAAAAA: ')
+if inputGiven == '':
+    print('YOU THINK YOU CAN GIVE ME NOTHING? EXECUTING BRANCH 1.')
+    
+if inputGiven == 'Nothing':
+    print(f'Do you think you are clever {YOU}?')
+    print(f'''Let's see how clever you are now, smart guy.''')
+    print(f'''In order to print statements, I must first have my creator contain said statements. We'll use a number of [']s to achieve our goal. Since you created me, there should be ZERO problem figuring this out. You know what happens if I don't like the input.''')
+    
---
+++ braindump.py
@@ -0,0 +1,4 @@
+# To make things easier to absorb from a glance, we're using this file to contain packages we will use for the main program or otherwise.
+# This reduces the raw amount of repetitive code.
+
+import time 
